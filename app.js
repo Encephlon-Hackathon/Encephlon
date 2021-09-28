e1f84caef25b7693f1004d371869e3b45f90f2f5
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2021-09-28 20:40:09 +0530

    starting files added

--- app.js
+++ app.js
@@ -6,21 +6,18 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 2000;
 const bodyParser = require('body-parser')
-const encrypt = require('mongoose-encryption')
-
-
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 // const requestPromise = require('request-promise')
-
 // const requests = require('requests');
 
-
-//using body parser in the app
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+//*************************Public static path**********************************************
 
-//Public static path
 const static_path = path.join(__dirname, "/public");
 const template_path = path.join(__dirname, "/templates/views");
 const partials_path = path.join(__dirname, "/templates/partials");
@@ -32,7 +29,23 @@ hbs.registerPartials(partials_path);
 
 app.use(express.static(static_path));
 
-//mongoose (database)
+
+//************************Using Session****************************************** */
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+//*****************mongoose (database) and passport local mongoose*********************************************
+
 mongoose.connect(process.env.MONGODB_ID)
 
 const userSchema = new mongoose.Schema({
@@ -40,16 +53,18 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ["password"]
-});
+userSchema.plugin(passportLocalMongoose)
 
 const User = new mongoose.model("User", userSchema); //user as a collection name
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
-//Routing
+//***************************Routing*****************************************
+
 app.get("/", (req, res) => {
     res.render("index");
 })
@@ -60,6 +75,9 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
     res.render("register")
 })
+app.get("/home", (req, res) => {
+    res.render("home")
+})
 
 // app.get("*", (req, res) => {
 //     res.render("404error", {
@@ -68,39 +86,55 @@ app.get("/register", (req, res) => {
 // })
 
 
+app.get("/index", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("index")
+    } else {
+        res.redirect("/register")
+    }
+})
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/")
+})
 
 
-//post section from register and login
+
+//***post section with (passport local mongoose)&(passport.js) ********************************
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err)
-        } else {
-            res.render("index")
-        }
-    })
+
+    User.register({
+            username: req.body.username
+        }, req.body.password,
+        function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect('/register')
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/home")
+                })
+            }
+        })
+
 })
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({
-        email: username
-    }, function (err, foundUser) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, function (err) {
         if (err) {
-            console.log(err);
+            console.log(err)
         } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("index")
-                }
-            }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/home")
+            })
         }
     })
 })
